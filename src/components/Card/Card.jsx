2d68f597b23c7b7829087ae3337c356c1bc05adc
alejandro <alejandro.morel1905@gmail.com>
2023-08-30 12:14:27 -0300

    initial structure

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    <div className='container'>
+      <h1>WEATHER APP</h1>
+      <form>
+        <input type='text' />
+      </form>
+    </div>
+  );
+};
+
+export default Card;
